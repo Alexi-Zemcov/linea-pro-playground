d02d4a39d616e96e1f4dc1311b43c4ed2c1bb44e
Alexey Zemcov <alexi.zemcov@gmail.com>
2022-05-26 15:29:01 +0300

    first commit

--- lineaproplayground/ViewController.swift
+++ lineaproplayground/ViewController.swift
@@ -7,13 +7,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, DTDeviceDelegate {
+    
+    private let scanner : DTDevices = DTDevices()
+    
+    private let button : UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .gray
+        button.setTitle("Check state", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
+        self.scanner.delegate = self
+        self.scanner.connect()
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(button)
+        button.addTarget(
+            self,
+            action: #selector(didTapButton),
+            for: .touchUpInside
+        )
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(
+            x: 30,
+            y:view.frame.size.height-65-view.safeAreaInsets.bottom,
+            width: view.frame.size.width-60,
+            height: 50
+        )
+    }
+    
+    @objc func didTapButton(){
+         checkConnectionState()
+     }
+    func checkConnectionState(){
+        let variable = self.scanner.btConnectedDevices
+        
+        
+        
+        if self.scanner.connstate == 2 {
+            /*
+                CONNECTED
+            */
+            self.button.setTitle("Connected", for: .normal)
+        } else if self.scanner.connstate == 1{
+            /*
+                NOT CONNECTED AND WILL NOT TRY TO EITHER (Your accessory is probably not attached
+            */
+            self.button.setTitle("Not connected at all", for: .normal)
+        } else {
+            /*
+                CONNECTING
+            */
+            self.button.setTitle("Trying to connect", for: .normal)
+        }
+    }
+    
 }
 
